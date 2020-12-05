f28994d5061f58b5d2f73daf1117fef51710efac
Gabriel Portuga <gabriel.port@gmail.com>
2020-12-05 17:40:55 -0300

    first commit

---
+++ src/test/java/com/portuga/springbootex/SpringbootexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portuga.springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
