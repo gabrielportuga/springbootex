b574ebdb8504255a02dcc082393432fb6b8620f6
Gabriel Portuga <gabriel.port@gmail.com>
2020-12-05 22:36:34 -0300

    exception handling

--- src/main/java/com/portuga/springbootex/services/CategoryService.java
+++ src/main/java/com/portuga/springbootex/services/CategoryService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.portuga.springbootex.domain.Category;
 import com.portuga.springbootex.repositories.ICatergoryRepository;
+import com.portuga.springbootex.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
 
 	@Autowired
 	private ICatergoryRepository repo;
-	
+
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
 }
